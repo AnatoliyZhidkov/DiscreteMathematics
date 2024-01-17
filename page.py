74c4f987ac83ad24e4eb4d7d47fdd4de37a44915
AnatoliyZhidkov <Anatoliyzhudkov@yandex.ru>
2024-01-17 19:00:14 +0300

    fix

--- page.py
+++ page.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for,request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["POST","GET"])
 def index():
+    if request.method == 'POST':
+        print(request.form)
     return render_template('diplom.html')
 
 @app.route("/about")
